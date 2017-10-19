416be3ab78ffc5ebaa357dc711e97143e2e555de
Sam Shokrollahi <sam.shokrollahi@pcc.edu>
2017-10-19 11:38:04 -0700

    print date

--- src/CIS233j_HelloWorld/HelloWorld.java
+++ src/CIS233j_HelloWorld/HelloWorld.java
@@ -1,7 +1,10 @@
 package CIS233j_HelloWorld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main (String args[]){
         System.out.println("hello world");
+        System.out.println(new Date());
     }
 }
